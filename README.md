272a990655e0947e66aebe5f9293583158889160
David Karpay <dlkarpay@gmail.com>
2014-01-07 14:16:25 -0500

    initial commit!

---
+++ README.md
@@ -0,0 +1,4 @@
+# Sample App from Michael Hartl's Rails Tutorial
+# Chapter 3 -- Static Pages (& Testing with RSpec)
+
+If you saw this, [send me an email](mailto:dlkarpay@gmail.com).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'ab675a4daae5f54172e1a3a6df8800120701b3fcaa55d2ad5839c5b11c49f9f317b116c8d3e83531669e427a64e6b0ef670ef432477373d62f1fb8cf9f32159c'
