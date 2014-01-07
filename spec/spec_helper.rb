509da4e5d147038e5b7ef46812c2dbb7f0fe8692
David Karpay <dlkarpay@gmail.com>
2014-01-07 17:27:58 -0500

    Updated spec/spec_helper.rb to get Capybara::DSL

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,8 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # Added to get Capybara DSL, which lets you run tests created with the command:
+  #             '$ rails g integration_test <controller_name>'
+  config.include Capybara::DSL
 end
