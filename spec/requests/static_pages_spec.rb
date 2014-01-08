70470caad05f606268229ea15603e582b64472e8
David Karpay <dlkarpay@gmail.com>
2014-01-08 15:00:33 -0500

    added static_pages contact test for title content and body content

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -50,6 +50,11 @@ describe "StaticPages" do
       expect(page).to have_content('Contact')
     end
 
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  
   end
 
 end
