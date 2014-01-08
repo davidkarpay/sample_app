da45b105fe576d7fb12bd179ee4dc34b75447d39
David Karpay <dlkarpay@gmail.com>
2014-01-08 14:48:10 -0500

    added Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,12 +6,12 @@ describe "StaticPages" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      expect(page).to have_content ('Sample App')
+      expect(page).to have_content('Sample App')
     end
 
     it "should have the title 'Home'" do
     	visit '/static_pages/home'
-    	expect(page).to have_title ("Ruby on Rails Tutorial Sample App | Home")
+    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
   	end
   
   end
@@ -20,12 +20,12 @@ describe "StaticPages" do
   
   	it "should have the content 'Help'" do
   		visit '/static_pages/help'
-  		expect(page).to have_content ('Help')
+  		expect(page).to have_content('Help')
  		end
 
  		it "should have the title 'Help'" do
  			visit '/static_pages/help'
- 			expect(page).to have_title ("Ruby on Rails Tutorial Sample App | Help")
+ 			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
   	end
   
   end
@@ -34,13 +34,22 @@ describe "StaticPages" do
   
   	it "should have the content 'About Us'" do
   		visit '/static_pages/about'
-  		expect(page).to have_content ('About Us')
+  		expect(page).to have_content('About Us')
   	end
   
   	it "should have the title 'About Us'" do
   		visit '/static_pages/about'
-  		expect(page).to have_title ("Ruby on Rails Tutorial Sample App | About Us")
+  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
   	end
+  end
+
+  describe "Contact" do 
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
   end
 
 end
