a79cb4d8edeebbe733e38dfea33c672e58a7182c
David Karpay <dlkarpay@gmail.com>
2014-01-11 01:14:22 -0500

    fixed routes issue -- it wasnt an issue, i was just being stupid

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 SampleApp::Application.routes.draw do
-  root to: 'static_pages#home' #matches '/'
-  
-  match '/help' => 'static_pages#help', via: [:get, :post]
+# root to: 'static_pages#home' #matches '/'
+  root :to => 'static_pages#home'
+
+  match '/help', to: 'static_pages#help', via: [:get, :post]
   match '/about', to: 'static_pages#about', via: [:get, :post]
   match '/contact', to: 'static_pages#contact', via: [:get, :post]
   # The priority is based upon order of creation: first created -> highest priority.
