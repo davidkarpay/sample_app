90891843b39056e246c715ea2d9616098dc4374e
David Karpay <dlkarpay@gmail.com>
2014-01-13 13:05:17 -0500

    added explanatory comments on how to use methods in tests

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,9 @@
+# Even though this method is defined in 
+#     app/helpers/application_helper.rb, 
+# in order to use methods in int. tests,
+# (and if those methods are not defined
+# in the test itself) the methods must be
+# defined in this file.
 def full_title(page_title)
 	base_title = "Ruby on Rails Tutorial Sample App"
 	if page_title.empty?
