6a4db21272b61b7fd18f065e5bbe5c2f0733f3c5
David Karpay <dlkarpay@gmail.com>
2014-01-13 13:47:01 -0500

    created a mock applicationhelper with regexps

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -4,11 +4,14 @@
 # (and if those methods are not defined
 # in the test itself) the methods must be
 # defined in this file.
-def full_title(page_title)
-	base_title = "Ruby on Rails Tutorial Sample App"
-	if page_title.empty?
-		base_title
-	else
-		"#{base_title} | #{page_title}"
-	end
-end
+
+#   def full_title(page_title)
+#   	base_title = "Ruby on Rails Tutorial Sample App"
+#   	if page_title.empty?
+#  	 	base_title
+#  	 else
+#  	 	"#{base_title} | #{page_title}"
+#  	 end
+#   end
+
+include ApplicationHelper
