5e27fbddd2cf80644ec22413581e6bf77e08f151
David Karpay <dlkarpay@gmail.com>
2014-01-14 15:17:09 -0500

    downcasing user.email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
 	attr_accessible :name, :email
 # attr_accessible(:name, :email)
+
+	before_save { |user| user.email = email.downcase }
+# sets user's email to lowercase version of its value
 	validates :name, presence: true, length: { maximum: 50 }
 # validates(:name, {:presence => true}, {:length => {:maximum => 50}})
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
