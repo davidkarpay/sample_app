3a69bb475ba2aa7e9e0791fbfee4fda574b3a60b
David Karpay <dlkarpay@gmail.com>
2014-01-13 19:16:48 -0500

    added name length test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,9 +10,20 @@ describe User do
 	it { should respond_to(:email) }
 
 	it { should be_valid } # verifies @user obj is initially valid
+# When obj responds to valid?, corresponding test method 'be_valid'
 
 	describe "when name is not present" do
-		before { @user.name = " " }
-		it { should_not be_valid }
+		before { @user.name = " " } # Invalidates @user
+		it { should_not be_valid } # Test for invalidation
+	end
+
+	describe "when email is not present" do
+		before { @user.email = " " } # Invalidates @user
+		it { should_not be_valid } # Tests for invalidation
+	end
+
+	describe "when name is too long" do
+		before { @user.name = "a"*51 } # Name too long
+		it { should_not be_valid } # Tests for invalidation
 	end
 end
