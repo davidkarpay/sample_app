37763db43755edbacf9a43b236321f05f9d35440
David Karpay <dlkarpay@gmail.com>
2014-01-14 16:22:32 -0500

    password length and presence validations; password_confirmation presence validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	attr_accessible :name, :email
+	attr_accessible :name, :email, :password, :password_confirmation
 # attr_accessible(:name, :email)
 
 	before_save { |user| user.email = email.downcase }
@@ -9,5 +9,8 @@ class User < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 # validates(:email, {:presence => true}, {:f => {:w, V}}...)
-
+	validates :password, presence: true, length: { minimum: 6 }
+	#
+	validates :password_confirmation, presence: true
+	#
 end
