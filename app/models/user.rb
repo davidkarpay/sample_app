3f918ee9dcf6e52df1229d633d144cdadda8ab7a
David Karpay <dlkarpay@gmail.com>
2014-01-15 12:02:06 -0500

    refactored email downcasing before_save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,17 @@
 class User < ActiveRecord::Base
 	attr_accessible :name, :email, :password, :password_confirmation
-# attr_accessible(:name, :email)
+
 	has_secure_password
 
-	before_save { |user| user.email = email.downcase }
-# sets user's email to lowercase version of its value
+	before_save { self.email.downcase! }# sets user's email to lowercase version of its value
+
 	validates :name, presence: true, length: { maximum: 50 }
-# validates(:name, {:presence => true}, {:length => {:maximum => 50}})
+
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-# validates(:email, {:presence => true}, {:f => {:w, V}}...)
+
 	validates :password, presence: true, length: { minimum: 6 }
-	#
+	
 	validates :password_confirmation, presence: true
-	#
+
 end
