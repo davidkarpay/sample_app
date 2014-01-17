f42687f377def60c785cc6ca31a2bd503fed3d32
David Karpay <dlkarpay@gmail.com>
2014-01-17 00:11:18 -0500

    edited @user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user - User.new(params[:user])
+  	@user = User.new(params[:user])
   	if @user.save
   		# Handle a successful save.
   	else
